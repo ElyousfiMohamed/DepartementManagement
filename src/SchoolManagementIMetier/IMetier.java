12c2a9abc7c6bf1c0c6ace4a20b917da6f421b01
“[Mohamed <[mohamed5elyousfi@gmail.com]>
2021-12-28 12:03:57 +0100

    IMetier + SingletonConnexionDB

--- src/SchoolManagementIMetier/IMetier.java
+++ src/SchoolManagementIMetier/IMetier.java
@@ -4,10 +4,21 @@
  */
 package SchoolManagementIMetier;
 
+import java.util.List;
+import persistantClasse.Departement;
+import persistantClasse.Professeur;
+
 /**
  *
  * @author ELYOUSFI
  */
 public interface IMetier {
+    void addProfesseur(Professeur c);
+    void delProfesseur(Professeur c);
+    List<Professeur> getAllProfesseurs();
+    void addProfToDepart();
     
+    void addDepartement(Departement c);
+    void delDepartement(Departement c);
+    List<Departement> getAllDepartements();
 }
